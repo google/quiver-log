3d2a272225f4500cb4b8a076a56bf392dbf6d8e5
Christopher Wright <dhasenan@google.com>
2014-04-29 21:27:48 +0000

    split out controller, some tests

---
+++ test/logviewer_test.dart
@@ -0,0 +1,79 @@
+library quiver.log.logviewer_test;
+
+import 'dart:async';
+import 'package:logging/logging.dart';
+import 'package:quiver-log/logviewer/logviewer.dart';
+import 'package:unittest/unittest.dart';
+
+main() {
+  group('Filter', () {
+    group('Parsing', () {
+      test('min date', () {
+        var dateString = '2014-03-17T05:19:03.000Z';
+        var filters = Filter.parseAll("mindate:$dateString");
+        expect(filters.length, equals(1));
+        var filter = filters[0];
+        expect(filter.toString(), equals("mindate:${dateString}"));
+      });
+
+      test('max date', () {
+        var dateString = '2014-03-17T05:19:03.000Z';
+        var filters = Filter.parseAll("maxdate:$dateString");
+        expect(filters.length, equals(1));
+        var filter = filters[0];
+        expect(filter.toString(), equals("maxdate:${dateString}"));
+      });
+
+      test('file.dart', () {
+        var filters = Filter.parseAll('file:foo.dart');
+        expect(filters.length, equals(1));
+        var filter = filters[0];
+        expect(filter.toString(), equals('file:foo.dart'));
+      });
+
+      test('file', () {
+        var filters = Filter.parseAll('file:foo');
+        expect(filters.length, equals(1));
+        var filter = filters[0];
+        expect(filter.toString(), equals('file:foo.dart'));
+      });
+
+      test('logger name', () {
+        var filters = Filter.parseAll('logger:foo');
+        expect(filters.length, equals(1));
+        var filter = filters[0];
+        expect(filter.toString(), equals('logger:foo'));
+      });
+    });
+
+    group('Date matching', () {
+      test('min date, record too late', () {
+        var logRecord = new LogRecord();
+        var date = new DateTime.now();
+        var filter = Filter.parseAll("mindate:${date.toIso8601String()}")[0];
+        expect(filter.match(logRecord), isFalse);
+      });
+
+      test('min date, record early enough', () {
+        var date = new DateTime.now();
+        var logRecord = new LogRecord();
+        var filter = Filter.parseAll("mindate:${date.toIso8601String()}")[0];
+        expect(filter.match(logRecord), isTrue);
+      });
+
+      test('max date, record late enough', () {
+        var logRecord = new LogRecord();
+        var date = new DateTime.now();
+        var filter = Filter.parseAll("maxdate:${date.toIso8601String()}")[0];
+        expect(filter.match(logRecord), isTrue);
+      });
+
+      test('max date, record too early', () {
+        var date = new DateTime.now();
+        var logRecord = new LogRecord();
+        var filter = Filter.parseAll("maxdate:${date.toIso8601String()}")[0];
+        expect(filter.match(logRecord), isFalse);
+      });
+    });
+  });
+}
