64b0b6ece88f6166d64686b43ee425fe30b7844f
Adam Bender <adambender@gmail.com>
2021-02-12 19:05:14 -0800

    fixing up more merge stuff

---
+++ changelog.md
@@ -0,0 +1,10 @@
+# Changelog 
+## 2.0.0-nullsafety.0 - 02/12/2021
+- Updating for null safety and cleaning up basic package health.
+
+## 1.0.7 - 10/22/2019
+- Introduce the changelog.
+- Improve handling of errors during formatting or appending by attempting to relog with safer alternatives.
+- Remove generics from API as all users appear to be using <String>. This also made it easier to handle re-logging.
+- Migrated to Dart 2 syntax.
+
--- test/webappender_test.dart
+++ test/webappender_test.dart
@@ -22,73 +22,73 @@ import 'package:quiver_log/web.dart';
 import 'package:test/test.dart';
 
 void main() {
-  WebAppender? webAppender;
-  Logger? logger;
-  FakeConsole? fakeConsole;
+  late WebAppender webAppender;
+  late Logger logger;
+  late FakeConsole fakeConsole;
 
   group('WebAppender', () {
     setUp(() {
       Logger.root.level = Level.ALL;
       logger = Logger('testlogger');
       fakeConsole = FakeConsole();
-      webAppender = WebAppender.usingConsole(NoopFormatter(), fakeConsole!);
-      webAppender?.attachLogger(logger!);
+      webAppender = WebAppender.usingConsole(NoopFormatter(), fakeConsole);
+      webAppender.attachLogger(logger);
     });
 
     test('Uses correct console methods for config level', () {
-      expect(fakeConsole!.logMessages.length, equals(0));
-      logger!.config('config message');
-      expect(fakeConsole!.logMessages.length, equals(1));
-      expect(fakeConsole!.logMessages.last, equals('config message'));
+      expect(fakeConsole.logMessages.length, equals(0));
+      logger.config('config message');
+      expect(fakeConsole.logMessages.length, equals(1));
+      expect(fakeConsole.logMessages.last, equals('config message'));
     });
 
     test('Uses correct console methods for finest level', () {
-      expect(fakeConsole!.logMessages.length, equals(0));
-      logger!.finest('finest message');
-      expect(fakeConsole!.logMessages.length, equals(1));
-      expect(fakeConsole!.logMessages.last, equals('finest message'));
+      expect(fakeConsole.logMessages.length, equals(0));
+      logger.finest('finest message');
+      expect(fakeConsole.logMessages.length, equals(1));
+      expect(fakeConsole.logMessages.last, equals('finest message'));
     });
 
     test('Uses correct console methods for finer level', () {
-      expect(fakeConsole!.logMessages.length, equals(0));
-      logger!.finer('finer message');
-      expect(fakeConsole!.logMessages.length, equals(1));
-      expect(fakeConsole!.logMessages.last, equals('finer message'));
+      expect(fakeConsole.logMessages.length, equals(0));
+      logger.finer('finer message');
+      expect(fakeConsole.logMessages.length, equals(1));
+      expect(fakeConsole.logMessages.last, equals('finer message'));
     });
 
     test('Uses correct console methods for fine level', () {
-      expect(fakeConsole!.logMessages.length, equals(0));
-      logger!.fine('fine message');
-      expect(fakeConsole!.logMessages.length, equals(1));
-      expect(fakeConsole!.logMessages.last, equals('fine message'));
+      expect(fakeConsole.logMessages.length, equals(0));
+      logger.fine('fine message');
+      expect(fakeConsole.logMessages.length, equals(1));
+      expect(fakeConsole.logMessages.last, equals('fine message'));
     });
 
     test('Uses correct console methods for info level', () {
-      expect(fakeConsole!.infoMessages.length, equals(0));
-      logger!.info('info message');
-      expect(fakeConsole!.infoMessages.length, equals(1));
-      expect(fakeConsole!.infoMessages.last, equals('info message'));
+      expect(fakeConsole.infoMessages.length, equals(0));
+      logger.info('info message');
+      expect(fakeConsole.infoMessages.length, equals(1));
+      expect(fakeConsole.infoMessages.last, equals('info message'));
     });
 
     test('Uses correct console methods for warning level', () {
-      expect(fakeConsole!.warnMessages.length, equals(0));
-      logger!.warning('warn message');
-      expect(fakeConsole!.warnMessages.length, equals(1));
-      expect(fakeConsole!.warnMessages.last, equals('warn message'));
+      expect(fakeConsole.warnMessages.length, equals(0));
+      logger.warning('warn message');
+      expect(fakeConsole.warnMessages.length, equals(1));
+      expect(fakeConsole.warnMessages.last, equals('warn message'));
     });
 
     test('Uses correct console methods for severe level', () {
-      expect(fakeConsole!.errorMessages.length, equals(0));
-      logger!.severe('severe message');
-      expect(fakeConsole!.errorMessages.length, equals(1));
-      expect(fakeConsole!.errorMessages.last, equals('severe message'));
+      expect(fakeConsole.errorMessages.length, equals(0));
+      logger.severe('severe message');
+      expect(fakeConsole.errorMessages.length, equals(1));
+      expect(fakeConsole.errorMessages.last, equals('severe message'));
     });
 
     test('Uses correct console methods for shout level', () {
-      expect(fakeConsole!.errorMessages.length, equals(0));
-      logger!.shout('shout message');
-      expect(fakeConsole!.errorMessages.length, equals(1));
-      expect(fakeConsole!.errorMessages.last, equals('shout message'));
+      expect(fakeConsole.errorMessages.length, equals(0));
+      logger.shout('shout message');
+      expect(fakeConsole.errorMessages.length, equals(1));
+      expect(fakeConsole.errorMessages.last, equals('shout message'));
     });
   });
 }
